7118147b128de34899803acb457517f7a84efc21
Sahidul Islam <7229757+shaaheed@users.noreply.github.com>
2025-11-04 23:32:08 +0600

    Update text_style.extension.dart

--- lib/extensions/text_style.extension.dart
+++ lib/extensions/text_style.extension.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 extension TextStyleExtension on TextStyle {
   static TextStyle errorStyle(BuildContext context) {
-    return TextStyle(color: Theme.of(context).errorColor);
+    return TextStyle(color: Theme.of(context).colorScheme.error);
   }
 }
