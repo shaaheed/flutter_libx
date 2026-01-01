a6d4643803a73d12cc7121574193bbe48bcea81d
Sahidul Islam <7229757+shaaheed@users.noreply.github.com>
2026-01-01 12:59:36 +0600

    Create const.dart

---
+++ lib/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Const {
+  static const EdgeInsetsGeometry appPagePadding = EdgeInsets.only(
+    top: 35.0,
+    left: 10,
+    right: 20.0,
+  );
+}
