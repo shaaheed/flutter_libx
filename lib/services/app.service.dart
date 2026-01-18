01993676a6f44e135ff0a6568f8ad7822be9ce49
Sahidul Islam <shahidcse6@gmail.com>
2026-01-19 00:37:09 +0600

    update share lib

--- lib/services/app.service.dart
+++ lib/services/app.service.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './toast.service.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
 import '../extensions/string.extension.dart';
 
